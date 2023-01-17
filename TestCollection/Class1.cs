d1a9444b3d957c4d2b1124be875e4928703a9c9c
SmutnaZabajpg <99398267+SmutnaZabajpg@users.noreply.github.com>
2023-01-17 09:48:03 +0100

    Add files via upload

---
+++ TestCollection/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCollection
+{
+ 
+    public class Square
+    {
+        protected double sideA;
+        
+        public Square()
+        {
+
+        }
+
+        public Square(double side)
+        {
+            sideA = side;
+        }
+        
+        public void ShowInfo()
+        {
+           
+            Console.WriteLine("Informacje o obiekcie:");
+            Console.WriteLine($"Bok: {sideA}");
+            Console.WriteLine($"Pole: {sideA * sideA}");
+            Console.WriteLine($"Obwód: {sideA * 4}");
+        }
+    }
+    public class Rectangle : Square
+    {
+        protected double sideB;
+
+        public Rectangle()
+        {
+            sideA = 1;
+            sideB = 1;
+        }
+
+        public Rectangle(double sideA, double sideB)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+        }
+
+        public new void ShowInfo()
+        {
+
+            Console.WriteLine("Informacje o obiekcie:");
+            Console.WriteLine($"Boki: {sideA}, {sideB}");
+            Console.WriteLine($"Pole: {sideA * sideB}");
+            Console.WriteLine($"Obwód: {sideA * 2 + sideB * 2}");
+        }
+    }
+}
---
+++ TestCollection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Intrinsics.X86;
+
+namespace TestCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square(5);
+            Square rectangle = new Rectangle(2, 5);
+            square.ShowInfo();
+           
+        }
+    }
+}
